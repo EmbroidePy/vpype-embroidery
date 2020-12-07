ca8bb18304b0fe24b771d0c7e6f525a2043ad445
tatarize <tatarize@gmail.com>
2020-12-06 17:56:51 -0800

    0.0.5

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
     name="vpype-embroidery",
-    version="0.0.2",
+    version="0.0.5",
+    description="vpype embroidery plugin",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Tatarize",
     author_email="tatarize@gmail.com",
-    description="vpype embroidery plugin",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/embroidepy/vpype-embroidery/",
-    packages=setuptools.find_packages(),
+    packages=["vpype_embroidery"],
     classifiers=(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -26,9 +27,9 @@ setuptools.setup(
         "pyembroidery",
     ],
     entry_points="""
-        [vpype.plugins]
-        eread=vpype_embroidery.eread:eread
-        ewrite=vpype_embroidery.ewrite:ewrite
-        efill=vpype_embroidery.efill:efill
-    """,
+            [vpype.plugins]
+            read_emb=vpype_embroidery.read_emb:read_emb
+            write_emb=vpype_embroidery.write_emb:write_emb
+            efill=vpype_embroidery.efill:efill
+        """,
 )
